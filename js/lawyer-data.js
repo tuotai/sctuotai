e27d1f8e52a25a03d5c74ca765617cc9be33fdf7
tuotai <heiwandao@126.com>
2026-05-04 14:37:39 +0800

    更新律师数据

---
+++ js/lawyer-data.js
@@ -0,0 +1,205 @@
+var lawyerData = [
+  {
+    "id": "1",
+    "name": "吴苛政",
+    "title": "主任律师",
+    "photo": "https://picsum.photos/seed/lawyer1/400/500",
+    "email": "wukz@sctuotai.com",
+    "contact": "13800138000",
+    "introduction": "吴苛政律师，四川拓泰律师事务所创始合伙人、主任律师。拥有超过15年律师执业经验，专注于企业法律服务、金融证券、并购重组等领域。",
+    "specialties": [
+      "企业法律服务",
+      "金融证券",
+      "并购重组"
+    ],
+    "expertise": [
+      "企业法律顾问服务",
+      "公司并购与重组",
+      "金融证券法律服务",
+      "合同纠纷处理",
+      "股权架构设计",
+      "企业合规审查"
+    ],
+    "education": "西南政法大学 法学硕士",
+    "experience": "15年执业经验",
+    "cases": 200
+  },
+  {
+    "id": "2",
+    "name": "黄礼",
+    "title": "高级合伙人",
+    "photo": "https://picsum.photos/seed/lawyer2/400/500",
+    "email": "huangli@sctuotai.com",
+    "contact": "13900139000",
+    "introduction": "黄礼律师，四川拓泰律师事务所高级合伙人。专注于建设工程、房地产、招投标等领域，具有丰富的实践经验。",
+    "specialties": [
+      "建设工程",
+      "房地产",
+      "招投标"
+    ],
+    "expertise": [
+      "建设工程合同纠纷",
+      "房地产开发与交易",
+      "招标投标法律事务",
+      "工程索赔与反索赔",
+      "工程造价纠纷",
+      "建筑企业法律顾问"
+    ],
+    "education": "四川大学 法学学士",
+    "experience": "12年执业经验",
+    "cases": 180
+  },
+  {
+    "id": "3",
+    "name": "王馨媛",
+    "title": "合伙人",
+    "photo": "https://picsum.photos/seed/lawyer3/400/500",
+    "email": "wangxy@sctuotai.com",
+    "contact": "13700137000",
+    "introduction": "王馨媛律师，四川拓泰律师事务所合伙人。专注于民商事诉讼、侵权纠纷、婚姻家事等领域，热心公益事业。",
+    "specialties": [
+      "民商事诉讼",
+      "侵权纠纷",
+      "婚姻家事"
+    ],
+    "expertise": [
+      "合同纠纷诉讼",
+      "人身损害赔偿",
+      "婚姻家庭纠纷",
+      "遗产继承纠纷",
+      "劳动争议处理",
+      "法律援助"
+    ],
+    "education": "西南财经大学 法学硕士",
+    "experience": "8年执业经验",
+    "cases": 150
+  },
+  {
+    "id": "4",
+    "name": "李明远",
+    "title": "合伙人",
+    "photo": "https://picsum.photos/seed/lawyer4/400/500",
+    "email": "limy@sctuotai.com",
+    "contact": "13600136000",
+    "introduction": "李明远律师，四川拓泰律师事务所合伙人。专注于知识产权、不正当竞争、商业秘密保护等领域。",
+    "specialties": [
+      "知识产权",
+      "不正当竞争",
+      "商业秘密"
+    ],
+    "expertise": [
+      "商标注册与维权",
+      "专利申请与保护",
+      "著作权侵权诉讼",
+      "商业秘密保护",
+      "反不正当竞争",
+      "知识产权战略规划"
+    ],
+    "education": "电子科技大学 知识产权法硕士",
+    "experience": "10年执业经验",
+    "cases": 120
+  },
+  {
+    "id": "5",
+    "name": "张守创",
+    "title": "合伙人",
+    "photo": "https://picsum.photos/seed/lawyer5/400/500",
+    "email": "zhangsc@sctuotai.com",
+    "contact": "13500135000",
+    "introduction": "张守创律师，四川拓泰律师事务所合伙人。专注于刑事辩护、刑事合规、企业反舞弊等领域。",
+    "specialties": [
+      "刑事辩护",
+      "刑事合规",
+      "企业反舞弊"
+    ],
+    "expertise": [
+      "刑事案件辩护",
+      "刑事合规审查",
+      "企业反舞弊调查",
+      "职务犯罪预防",
+      "刑事控告代理",
+      "企业刑事风险防控"
+    ],
+    "education": "中国政法大学 刑法学硕士",
+    "experience": "11年执业经验",
+    "cases": 90
+  },
+  {
+    "id": "6",
+    "name": "严俊",
+    "title": "合伙人",
+    "photo": "https://picsum.photos/seed/lawyer6/400/500",
+    "email": "yanjun@sctuotai.com",
+    "contact": "13400134000",
+    "introduction": "严俊律师，四川拓泰律师事务所合伙人。专注于行政法律、政府法律服务、政府采购等领域。",
+    "specialties": [
+      "行政法律",
+      "政府法律服务",
+      "政府采购"
+    ],
+    "expertise": [
+      "行政复议代理",
+      "行政诉讼代理",
+      "政府信息公开",
+      "政府采购法律服务",
+      "政府法律顾问",
+      "行政处罚听证"
+    ],
+    "education": "四川大学 宪法与行政法硕士",
+    "experience": "9年执业经验",
+    "cases": 110
+  },
+  {
+    "id": "7",
+    "name": "赵雅琪",
+    "title": "专职律师",
+    "photo": "https://picsum.photos/seed/lawyer7/400/500",
+    "email": "zhao yq@sctuotai.com",
+    "contact": "13300133000",
+    "introduction": "赵雅琪律师，四川拓泰律师事务所专职律师。专注于金融法律、银行不良资产处置、融资租赁等领域。",
+    "specialties": [
+      "金融法律",
+      "不良资产",
+      "融资租赁"
+    ],
+    "expertise": [
+      "银行法律事务",
+      "不良资产处置",
+      "融资租赁纠纷",
+      "保理业务法律",
+      "金融衍生品",
+      "金融合规"
+    ],
+    "education": "西南政法大学 民商法硕士",
+    "experience": "6年执业经验",
+    "cases": 80
+  },
+  {
+    "id": "8",
+    "name": "陈志强",
+    "title": "专职律师",
+    "photo": "https://picsum.photos/seed/lawyer8/400/500",
+    "email": "chenzq@sctuotai.com",
+    "contact": "13200132000",
+    "introduction": "陈志强律师，四川拓泰律师事务所专职律师。专注于公司法、股权纠纷、企业治理等领域。",
+    "specialties": [
+      "公司法",
+      "股权纠纷",
+      "企业治理"
+    ],
+    "expertise": [
+      "公司设立与变更",
+      "股权纠纷诉讼",
+      "公司治理结构",
+      "股东权益保护",
+      "公司解散清算",
+      "股权激励设计"
+    ],
+    "education": "电子科技大学 法律硕士",
+    "experience": "5年执业经验",
+    "cases": 70
+  }
+];
+
+function getAllLawyers() { return lawyerData; }
+function getLawyerById(id) { return lawyerData.find(l => l.id === id); }
